30937f18aa5ec3104a349cc27985e240b9b1d21e
Paul Tremblay <paulhtremblay@gmail.com>
2018-04-15 23:37:33 -0700

    code for sql indices

---
+++ big_data/index_test.py
@@ -0,0 +1,170 @@
+import os
+import pprint
+import csv
+
+import timeit
+import MySQLdb
+
+pp =pprint.PrettyPrinter(indent = 4)
+
+def write_csv(path, results):
+    with open(path, 'w') as write_obj:
+        writer = csv.DictWriter(write_obj, fieldnames=['num_rows', 'time'])
+        writer.writeheader()
+        for result in results:
+            writer.writerow(result)
+
+def _get_mysql_db(host, user, pw, db):
+    return MySQLdb.connect(host,
+        user, pw, db )
+
+def _create_one_tb(conn, primary_key = ''):
+    cursor = conn.cursor()
+    cursor.execute("""
+        drop table if exists test55 """)
+    cursor.close()
+    cursor = conn.cursor()
+    cursor.execute("""
+    create table test55
+    (pk int not null {primary_key} ,
+            name1 varchar(30) not null ,
+            name2 varchar(30) not null
+    )
+    """.format(primary_key = primary_key))
+    cursor.close()
+
+def _create_base_table(conn, path, table):
+    cursor = conn.cursor()
+    cursor.execute("""
+        drop table if exists {table};
+    """.format(table = table))
+    cursor.close()
+    cursor = conn.cursor()
+    cursor.execute("""
+    create table {table}
+    (pk int not null primary key ,
+            name1 varchar(30) not null ,
+            name2 varchar(30) not null
+    )
+    """.format(table = table))
+    cursor.close()
+    cursor = conn.cursor()
+    cursor.execute("""
+    load data local infile '{path}' into table {table}
+    fields terminated by ','
+    lines terminated by '\n'
+    """.format(table = table, path = path))
+    cursor.close()
+
+def _test_load_one_tb(cursor, table, base_table, number):
+    cursor.execute("""
+insert into {table}
+select * from {base_table} limit {number}
+;
+    """.format(table = table, number = number, base_table = base_table))
+
+def _create_temp_table(conn, table, base_table, num_rows, primary_key = ''):
+    cursor = conn.cursor()
+    cursor.execute("""
+        drop table if exists {table}
+    """.format(table = table))
+    cursor.close()
+    cursor = conn.cursor()
+    cursor.execute("""
+create table {table}
+(pk int not null {primary_key},
+	name1 varchar(30) not null ,
+	name2 varchar(30) not null
+)
+    """.format(table = table, primary_key = primary_key))
+    cursor.close()
+    cursor = conn.cursor()
+    cursor.execute("""
+insert into {table} 
+select * from {base_table} limit {number}
+;
+    """.format(table = table, number = num_rows, base_table = base_table))
+    cursor.close()
+
+def _create_temp_tables(conn, primary_key, num_rows):
+    _create_temp_table(conn, table = 'test11', base_table = 'test1', primary_key = primary_key,  num_rows = num_rows)
+    _create_temp_table(conn, table = 'test22', base_table = 'test2', primary_key = primary_key,  num_rows = num_rows)
+    _create_temp_table(conn, table = 'test33', base_table = 'test3', primary_key = primary_key,  num_rows = num_rows)
+
+
+def _creat_base_tables(conn):
+    _create_base_table(conn, 'test1.txt', 'test1')
+    _create_base_table(conn, 'test2.txt', 'test2')
+    _create_base_table(conn, 'test3.txt', 'test3')
+
+def max_query(cursor):
+    cursor.execute("""
+select max(t1.name1) 
+from test11 t1
+inner join test22 t2
+on t1.pk = t2.pk
+inner join test33 t3
+on t3.pk = t2.pk
+    """)
+
+
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped
+
+def _primary_key(conn):
+    result = []
+    verbose = True
+    for i in [1e5, 2e5, 3e5, 4e5, 6e5, 7e5, 8e5, 9e5, 1e6,  1.4e6, 1.8e6, 2e6,
+            ]:
+        if verbose:
+            print("creating tables for {i}".format(i = i))
+        _create_temp_tables(conn, 'Primary Key', int(i))
+        cursor = conn.cursor()
+        cursor.execute("""RESET QUERY CACHE""")
+        cursor.close()
+        cursor = conn.cursor()
+        wrapped = wrapper(max_query, cursor)
+        t = timeit.timeit(wrapped, number=1)
+        if verbose:
+            print("max took {t} seconds".format(t = round(t,1)))
+        cursor.close()
+        result.append({'num_rows':i, 'time': t})
+    write_csv('primary_key.csv', result)
+
+def _no_primary_key(conn):
+    result = []
+    verbose = True
+    for i in [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000,
+            15000, 20000, 25000, 30000, 60000
+            ]:
+        if verbose:
+            print("creating tables for {i}".format(i = i))
+        _create_temp_tables(conn, '', i)
+        if verbose:
+            print("created tables for {i}".format(i = i))
+        cursor = conn.cursor()
+        wrapped = wrapper(max_query, cursor)
+        t = timeit.timeit(wrapped, number=1)
+        cursor.close()
+        result.append({'num_rows':i, 'time': t})
+    write_csv('no_primary_key.csv', result)
+
+
+
+def main():
+    conn = _get_mysql_db(host = 'pi-oasis-dev-cluster.cluster-ctiutsvmu8ci.us-west-2.rds.amazonaws.com', 
+        user = 'admin', pw = os.environ['AURORA_DB_PASSWORD'], db = 'app_dev')
+    #_no_primary_key(conn)
+    #_primary_key(conn)
+    _create_one_tb(conn = conn, primary_key = '')
+    cursor = conn.cursor()
+    wrapped = wrapper( _test_load_one_tb, cursor, 'test55', 'test1', int(1e6))
+    t = timeit.timeit(wrapped, number=1)
+    print(t)
+    cursor.close()
+
+if __name__ == '__main__':
+    main()
+
---
+++ big_data/indices_graph.py
@@ -0,0 +1,62 @@
+import math
+from bokeh.plotting import figure, output_file, show
+from bokeh.io import export_png
+
+def _get_no_indices_data():
+    return  ([1000, 2000, 3000,
+    4000, 5000, 6000, 7000, 8000, 10000,
+    15000, 20000, 25000, 30000, 60000,],
+    [ 0.2, 0.6, 1.4, 2.1,
+    3.3, 4.8, 7.3, 10.4, 13.0, 24.8,
+    55.4, 70.1, 118.1, 398.5])
+
+def _get_indices_data():
+    return ([ 100000, 200000, 300000, 400000,
+    600000, 700000, 800000, 900000, 1000000,
+    1400000, 1800000 ],
+    [ 0.3, 4.0, 7.8, 11.4, 16.8, 20.9,
+    24.9, 23.4, 25.7, 39.3, 52.2]
+    )
+
+def no_indices():
+    no_index = _get_no_indices_data()
+    index = _get_indices_data()
+    output_file("indices_sql1.html")
+    p = figure(title="Without Indices", x_axis_label='num rows', y_axis_label='seconds', 
+            plot_width=1000, plot_height=1000)
+    p.line(no_index[0], no_index[1], line_width=2, color="blue",
+    legend="Actual")
+    fitted = [.00000025/2* x * x for x in no_index[0]]
+    p.line(no_index[0], fitted, line_width=2, color="red", legend="fitted")
+    show(p)
+
+def indices():
+    c = 2.01342786663877e-06
+    index = _get_indices_data()
+    """
+    f = []
+    for counter, i in enumerate(index[0]):
+        c = index[1][counter]/(i * math.log(i) * i)
+        f.append(c)
+
+    print(f)
+    return
+    """
+
+    output_file("indices_sql2.html")
+    p = figure(title="With Indices", x_axis_label='num rows', y_axis_label='seconds', 
+            plot_width=1000, plot_height=1000)
+    p.line(index[0], index[1], line_width=2, color="blue",
+    legend="Actual")
+    fitted = [math.log(x) * x * c  for x in index[0]]
+    p.line(index[0], fitted, line_width=2, color="red", legend="fitted")
+    show(p)
+
+
+def main():
+    #no_indices()
+    indices()
+
+if __name__ == '__main__':
+    main()
+

b975c130c96972b3d7cf7f3ff39531d71d31e5dd
Henry Tremblay <a6002538@bestbuy.com>
2018-06-24 12:27:28 -0700

    unknown changes

--- big_data/python_tools/big_data_tools/bokeh_tools/cheat_sheet.py
+++ big_data/python_tools/big_data_tools/bokeh_tools/cheat_sheet.py
@@ -2,8 +2,16 @@ import os
 from google.cloud import bigquery
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/a6002538/Documents/ad-tech-test-204121-b0a06f7251e6.json'
 client = bigquery.Client()
-client.query("""slelect * from foo""")
-
+#client.query("""slelect * from foo""")
 
 from bokeh.models import NumeralTickFormatter
-p.yaxis.formatter=NumeralTickFormatter(format="0,")
+from bokeh.plotting import figure, show, output_file
+#p.yaxis.formatter=NumeralTickFormatter(format="0,")
+#p.xaxis.formatter=NumeralTickFormatter(format="0,")
+
+# to get all graphs wrapper
+from big_data_tools.bokeh_tools.all_p import *
+
+
+p = figure(x_range=labels, plot_height=500,  title="my test title")
+print('{x:,}'.format(x = x))#commas
--- big_data/python_tools/big_data_tools/bokeh_tools/histogram.py
+++ big_data/python_tools/big_data_tools/bokeh_tools/histogram.py
@@ -2,12 +2,13 @@ from bokeh.plotting  import figure, show
 from bokeh.models import Legend
 import numpy
 
-def hist(nums, bins = None, legend =  None):
+def hist(nums, bins = None, legend =  None, p = None):
     if bins:
         hist, edges = numpy.histogram(nums, bins=bins)
     else:
         hist, edges = numpy.histogram(nums)
-    p = figure()
+    if not p:
+        p = figure()
     r = p.quad(top = hist, bottom=0, left=edges[:-1], right=edges[1:], alpha = .4)
     if legend !=  None:
         legend = Legend(items=[
@@ -15,7 +16,7 @@ def hist(nums, bins = None, legend =  None):
                 ], location=(10, 0))
         p.add_layout(legend, 'right')
         #legend.background_fill_color = "green"
-    show(p)
+    return p
 
 if __name__ == '__main__':
     hist([1,1,1,1,15,5,5, 10, 10,10,8], legend="my legend")

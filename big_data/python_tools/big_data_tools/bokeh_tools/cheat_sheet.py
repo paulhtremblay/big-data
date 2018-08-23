e4862c4d03c0999afef42bc5643610463e6fd5e3
Henry <paulhtremblay@gmail.com>
2018-08-23 08:18:07 -0700

    adding savoy filter example

--- big_data/python_tools/big_data_tools/bokeh_tools/cheat_sheet.py
+++ big_data/python_tools/big_data_tools/bokeh_tools/cheat_sheet.py
@@ -1,9 +1,15 @@
 import os
 from google.cloud import bigquery
+from bokeh.models import NumeralTickFormatter
+from bokeh.plotting import figure
+from bokeh.io import output_notebook, show
+
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/a6002538/Documents/ad-tech-test-204121-b0a06f7251e6.json'
 client = bigquery.Client()
 client.query("""slelect * from foo""")
 
 
-from bokeh.models import NumeralTickFormatter
 p.yaxis.formatter=NumeralTickFormatter(format="0,")
+
+p = figure(plot_width=800, plot_height=300, title="Title")
+

69806ce396fcaee371c6d718f2bcd11eced05312
Henry Tremblay <a6002538@bestbuy.com>
2019-04-24 17:23:43 -0700

    updating example

--- big_data/python_tools/example.py
+++ big_data/python_tools/example.py
@@ -1,3 +1,4 @@
+import datetime
 from bokeh.io import show
 from bokeh.plotting import figure
 from  big_data_tools.bokeh_tools.bar import bar
@@ -6,6 +7,7 @@ from  big_data_tools.bokeh_tools.box_plot import box_plot
 from  big_data_tools.bokeh_tools.prob_plot import prob_plot 
 from bokeh.layouts import gridplot
 from bokeh.models import NumeralTickFormatter
+from bokeh.models import DatetimeTickFormatter
 
 #needed just for example
 
@@ -32,6 +34,16 @@ p_simple_line.line([1, 2, 3, 4, 5, 6], [1000000, 2000000, 3000000, 3000000, 3000
         legend='line 1' )
 p_simple_line.yaxis.formatter=NumeralTickFormatter(format="0,")
 
+#p with date
+p_with_date = figure(title = "date_example")
+p_with_date.line([datetime.datetime(2019,1,1), datetime.datetime(2019,1,2), 
+    datetime.datetime(2019,1,3), datetime.datetime(2019,1,4), datetime.datetime(2019,1,5), datetime.datetime(2019,1,6)], 
+    [1, 2, 3, 4, 5, 6], 
+    line_width =2, 
+        legend='line with date' )
+p_with_date.xaxis.formatter=DatetimeTickFormatter()
+#see https://bokeh.pydata.org/en/latest/docs/reference/models/formatters.html
+
 
 # get probability density for a set of discret values
 discrete_values = [{'value':1, 'num_occurences':33},
@@ -51,7 +63,9 @@ p_qq_normal = prob_plot(np.random.normal(size = 1000), 'norm')
 p_qq_weibul = prob_plot(np.random.weibull(a = .5, size = 1000), 'weibull_min', sparams= (.5,))
 
 grid = gridplot([p_bar, p_hist, p_box, 
-    p_simple_line, p_discrete_pdf, p_qq_normal,
+    p_simple_line, 
+    p_with_date,
+    p_discrete_pdf, p_qq_normal,
     p_qq_weibul,
     ], 
     ncols = 2, height=500, width = 500)

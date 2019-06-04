18e83e9676a6ac3ce9eab3355f1540a2fd76ba92
Henry <paulhtremblay@gmail.com>
2019-06-04 01:15:52 -0700

    box plot improved, exercises

--- big_data/python_tools/big_data_tools/bokeh_tools/box_plot.py
+++ big_data/python_tools/big_data_tools/bokeh_tools/box_plot.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 import numpy as np
 from bokeh.plotting import figure
+import pandas as pd
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
@@ -36,21 +37,58 @@ def zip_data(cats, data):
             outs[i] = zip_data[5][counter]
     return zip_data, outs
 
-def box_plot(cats, data, p = None):
+def box_plot(data, cats = None, p = None, df_type = None):
     """
+    Simple List
+    ============
     cats: a list of labels
     data: a list of lists
     Example:
     cats = ['a', 'b']
     data = [[1, 2, 3], [4, 5, 6]]
+    
+    Data Frame
+    ============
+    data = df, df_type = 'index'
+    OR
+    data = df, df_type = 'columns'
+    
+    Pandas Series
+    ==============
+    data =  [df['s1'], df['s2'], df['s3']]
 
     Returns: p, the figure()
     """
-    assert len(cats) == len(data)
-    assert isinstance(cats, list)
-    assert isinstance(data, list)
+    if isinstance(data, list) and isinstance(data[0], pd.core.series.Series ):
+        cats = []
+        for i in data:
+            cats.append(i.name)
+    elif isinstance(data, pd.core.frame.DataFrame):
+        assert df_type != None
+        if df_type == 'index':
+            cats = data.index.to_list()
+        elif df_type == 'columns':
+            cats = data.columns.to_list()
+        else:
+            raise ValueError('df_type must be "columns" or "index')
+        if df_type == 'index':
+            _data = []
+            for index, rows in data.iterrows():
+                _data.append(rows.values)
+            data = _data
+        elif df_type == 'columns':
+            _data = []
+            names = list(data)
+            for i in list(data):
+                _data.append(data[i].values)
+            data = _data
+    else:
+        assert len(cats) == len(data)
+        assert isinstance(cats, list)
+        assert isinstance(data, list)
     if not p:
         p = figure(tools="save", background_fill_color="#EFE8E2", x_range=cats)
     f, outs = zip_data(cats, get_quantiles(data))
     return make_box_raw(p, cats, f[0], f[1], f[2], f[3], f[4], outs)
+
 
